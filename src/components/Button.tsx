2c8697680c996382a6782c610f293bf5560c709a
FE4902 <isdn000123@gmail.com>
2023-07-17 23:45:22 +0900

    Feat: Button 추가

---
+++ src/components/Button.tsx
@@ -0,0 +1,47 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+import Variables from "styles/Variables";
+
+type ButtonType = {
+    color?: "default" | "primary" | "gray";
+    children: string;
+};
+
+const Button = (props: ButtonType) => {
+    const { color = "default", children } = props;
+
+    return <StyledButton color={color}>{children}</StyledButton>;
+};
+
+export default Button;
+
+// STYLE
+const colorStyles = {
+    default: css`
+        color: black;
+
+        &:hover {
+            background-color: #f5f5f5;
+        }
+    `,
+    primary: css`
+        color: white;
+        background-color: ${Variables.colors.primary};
+    `,
+    gray: css`
+        color: black;
+        background-color: ${Variables.colors.gray};
+    `,
+};
+
+const StyledButton = styled.button<ButtonType>`
+    padding: 8px 12px;
+    border-radius: 4px;
+
+    &:hover {
+        opacity: 0.75;
+    }
+
+    ${(props) => colorStyles[props.color || "default"]}
+`;
